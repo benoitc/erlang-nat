f07b66639228f997a8dd7c34de1c019e190ed1eb
macpie <pierre@helium.com>
2018-05-17 16:48:26 -0700

    Add `natupnp_v2` and fix scan if timeout

--- src/nat_scan.erl
+++ src/nat_scan.erl
@@ -29,6 +29,7 @@ start() ->
     ok = intercept:add(inet_ext, inet_ext_intercepts, [{{get_internal_address, 1}, get_internal_address}]),
 
     _ = natupnp_v1(),
+    _ = natupnp_v2(),
     _ = natpmp(),
 
     ok = intercept:clean(gen_udp),
@@ -69,8 +70,48 @@ natupnp_v1() ->
                 {error, R3} ->
                     ?LOG("[natupnp_v1] failed to get internal address ~p", [R3])
             end;
+        timeout ->
+            ?LOG("[natupnp_v1] failed to discover timeout", []);
         {error, _Reason} ->
             ?LOG("[natupnp_v1] failed to discover ~p", [_Reason])
+    end.
+
+natupnp_v2() ->
+    ?LOG("[natupnp_v2] discovering", []),
+    case natupnp_v2:discover() of
+        {ok, Context} ->
+            ?LOG("[natupnp_v2] discovered ~p", [Context]),
+
+            case natupnp_v2:add_port_mapping(Context, tcp, 8333, 8333, 3600) of
+                {ok, _Since, _InternalPort, _ExternalPort, _MappingLifetime}=OK ->
+                    ?LOG("[natupnp_v2] added port mapping ~p", [OK]),
+                    case natupnp_v2:delete_port_mapping(Context, tcp, 8333, 8333) of
+                        ok ->
+                            ?LOG("[natupnp_v2] deleted port mapping", []);
+                        {error, R1} ->
+                            ?LOG("[natupnp_v2] failed to delete port mapping ~p", [R1])
+                    end;
+                {error, R1} ->
+                    ?LOG("[natupnp_v2] failed to add port mapping ~p", [R1])
+            end,
+
+            case natupnp_v2:get_external_address(Context) of
+                {ok, ExtAddress} ->
+                    ?LOG("[natupnp_v2] got external address ~p", [ExtAddress]);
+                {error, R2} ->
+                    ?LOG("[natupnp_v2] failed to get external address ~p", [R2])
+            end,
+
+            case natupnp_v2:get_internal_address(Context) of
+                {ok, IntAddress} ->
+                    ?LOG("[natupnp_v2] got internal address ~p", [IntAddress]);
+                {error, R3} ->
+                    ?LOG("[natupnp_v2] failed to get internal address ~p", [R3])
+            end;
+        timeout ->
+            ?LOG("[natupnp_v2] failed to discover timeout", []);
+        {error, _Reason} ->
+            ?LOG("[natupnp_v2] failed to discover ~p", [_Reason])
     end.
 
 natpmp() ->
--- test/nat_debug_SUITE.erl
+++ test/nat_debug_SUITE.erl
@@ -12,6 +12,7 @@
 -export([
     natpmp/1
     ,natupnp_v1/1
+    ,natupnp_v2/1
 ]).
 
 -define(SCAN_FILE, "nat_scan").
@@ -27,7 +28,7 @@
 %% @end
 %%--------------------------------------------------------------------
 all() ->
-    [natpmp, natupnp_v1].
+    [natpmp, natupnp_v1, natupnp_v2].
 
 %%--------------------------------------------------------------------
 %% @public
@@ -118,6 +119,40 @@ natupnp_v1(_Config) ->
             nat:debug_start(?SCAN_FILE),
             ?assertEqual(GetIntAddress, natupnp_v1:get_internal_address(Context)),
             nat:debug_stop();
-        no_nat -> ok;
+        timeout -> ok;
+        {error, _Reason} -> ok
+    end.
+
+natupnp_v2(_Config) ->
+    Discover = natupnp_v2:discover(),
+    nat:debug_start(?SCAN_FILE),
+    ?assertEqual(Discover, natupnp_v2:discover()),
+    nat:debug_stop(),
+
+    case Discover of
+        {ok, Context} ->
+            AddPortMapping = natupnp_v2:add_port_mapping(Context, tcp, 8333, 8333, 3600),
+            nat:debug_start(?SCAN_FILE),
+            case AddPortMapping of
+                {ok, _Since, InternalPort, ExternalPortRequest, MappingLifetime} ->
+                    ?assertMatch(
+                        {ok, _, InternalPort, ExternalPortRequest, MappingLifetime}
+                        ,natupnp_v2:add_port_mapping(Context, tcp, 8333, 8333, 3600)
+                    );
+                Any ->
+                    ?assertEqual(Any, natupnp_v2:add_port_mapping(Context, tcp, 8333, 8333, 3600))
+            end,
+            nat:debug_stop(),
+
+            GetExtAddress = natupnp_v2:get_external_address(Context),
+            nat:debug_start(?SCAN_FILE),
+            ?assertEqual(GetExtAddress, natupnp_v2:get_external_address(Context)),
+            nat:debug_stop(),
+
+            GetIntAddress = natupnp_v2:get_internal_address(Context),
+            nat:debug_start(?SCAN_FILE),
+            ?assertEqual(GetIntAddress, natupnp_v2:get_internal_address(Context)),
+            nat:debug_stop();
+        timeout -> ok;
         {error, _Reason} -> ok
     end.
