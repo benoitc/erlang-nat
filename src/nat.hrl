a050b893461d9443076ddaebbf4d75376647db52
Benoit Chesneau <bchesneau@gmail.com>
2018-09-03 12:50:26 +0200

    set default recommanded lifetime to 7200
    follow RFC 6886 to set the default lifetime

--- src/nat.hrl
+++ src/nat.hrl
@@ -1,7 +1,7 @@
 -define(NAT_TRIES, 5).
 -define(NAT_INITIAL_MS, 250).
 
--define(RECOMMENDED_MAPPING_LIFETIME_SECONDS, 3600).
+-define(RECOMMENDED_MAPPING_LIFETIME_SECONDS, 7200).
 
 -record(nat_upnp, {
           service_url,
